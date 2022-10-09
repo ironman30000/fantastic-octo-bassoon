d9033f45fa7065f476ff97ec634e4065e7779263
groot3000 <91734414+groot3000@users.noreply.github.com>
2022-10-09 22:02:27 +0530

    Update c2.cpp

--- c2.cpp
+++ c2.cpp
@@ -27,3 +27,33 @@ int main()
     cin>>n>>m;
     
 }
+
+
+#pragma GCC optimize("O3", "unroll-loops")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+int s[100005],p[100005];
+int fin(int i)
+{
+    if(i==p[i])
+        return i;
+    return p[i]=fin(p[i]);
+}
+
+void mak(int a,int b)
+{
+    if(s[a]<s[b])
+        swap(a,b);
+    p[b]=a;
+    s[a]+=s[b];
+}
+
+int main()
+{       
+    
+    int n,m,x,y;
+    cin>>n>>m;
+    
+}
