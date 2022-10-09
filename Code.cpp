cf00df2d54ff6a9f2763b30773bebe8319ec2948
naamme <92879119+naamme@users.noreply.github.com>
2022-10-09 22:19:23 +0530

    Update Code.cpp

--- Code.cpp
+++ Code.cpp
@@ -8,6 +8,22 @@ using namespace std;
 #pragma GCC target("avx2")
 #include <bits/stdc++.h>
 using namespace std;
+int s[100005],p[100005];
+int fin(int i)
+{
+    if(i==p[i])
+        return i;
+    return p[i]=fin(p[i]);
+}
+
+void mak(int a,int b)
+{
+    if(s[a]<s[b])
+        swap(a,b);
+    p[b]=a;
+    s[a]+=s[b];
+}
+
 
 int s[100005],p[100005];
 int fin(int i)
